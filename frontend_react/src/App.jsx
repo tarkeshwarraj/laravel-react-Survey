cd8c86de76ae17488a5aca9ddd046365ab47b20a
tarkeshwar raj <tarkeshwarraj@gmail.com>
2024-09-19 11:56:51 +0530

    servey app

---
+++ frontend_react/src/App.jsx
@@ -0,0 +1,17 @@
+import reactLogo from './assets/react.svg' //This is in src folder so have to define path
+import viteLogo from '/vite.svg'  //This is image pic in public folder
+import './App.css'
+
+function App() {
+  return (
+    <>
+
+    <p className='bg-slate-600'>
+
+      nothing
+    </p>
+    </>
+  )
+}
+
+export default App
